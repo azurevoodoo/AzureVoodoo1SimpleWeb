df2afd9aff1ae7fd1ebaf8e949a1f2f977097158
Mattias Karlsson <matkar@wcom.se>
2016-01-23 15:58:48 +0100

    Web added

---
+++ src/SimpleWebApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleWebApplication")]
+[assembly: AssemblyDescription("Very simple Nancy Web")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WCOM AB")]
+[assembly: AssemblyProduct("SimpleWebApplication")]
+[assembly: AssemblyCopyright("Copyright WCOM AB ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd8e76a1-db09-4a5d-9985-434cbc93e2ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/SimpleWebApplication/WebModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace SimpleWebApplication
+{
+    public class WebModule : NancyModule
+    {
+        public WebModule()
+        {
+            Get["/"] = _ => "Welcome to the very simple web";
+        }
+    }
+}
